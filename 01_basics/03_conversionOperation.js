5b03984307660173659009946fb7872c9db76e84
Tinku Maji <120413794+tinkumaji@users.noreply.github.com>
2024-01-14 14:44:23 +0000

    conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -11,7 +11,7 @@ let convertScore = Number(score)
 // "33abc" => NaN
 // true => 1, false => 0
 // null => 0
-// undefined => NaN
+// undefined => NaN (Not a Number)
 
 let isLoggedIn = "Tinku" //1
 let booleanIsLogged = Boolean(isLoggedIn)
@@ -24,5 +24,48 @@ let booleanIsLogged = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringSomeNumber = String(someNumber)
-console.log(typeof stringSomeNumber);
-console.log(stringSomeNumber);
+// console.log(typeof stringSomeNumber);
+// console.log(stringSomeNumber);
+
+// ******************Operations**********************
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+// Basic operations:
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*4);
+// console.log(2**3); // ** => for finding power of a number
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " tinku"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log(1 + "2" + 2);
+// console.log(3 + 4 * 5 % 6);
+
+// These are bad programming practices, creates confusion
+console.log(+true); // -> 1
+console.log(-true); // -> -1
+console.log(+""); // -> 0
+console.log(-""); // -> -0
+console.log(+"a"); // -> NaN
+console.log(-"a"); // -> NaN
+
+let gameCounter = 100
+// gameCounter++
+++gameCounter
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
