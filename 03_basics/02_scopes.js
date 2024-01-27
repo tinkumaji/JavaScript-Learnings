918020a430ebda950aa9edd4cd613639a8302745
Tinku Maji <120413794+tinkumaji@users.noreply.github.com>
2024-01-27 06:08:37 +0000

    scopes basics

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -25,9 +25,50 @@ if (true) {
     // Doesn't change the global variable value
     let a = 10;
     const b =20;
-    console.log(a); // -> 10
-    console.log(b); // -> 20
+    // console.log(a); // -> 10
+    // console.log(b); // -> 20
 }
 
-console.log(a); // -> 100
-console.log(b); // -> 200
+// console.log(a); // -> 100
+// console.log(b); // -> 200
+
+
+function one() {
+    const username = "tinku";
+    function two() {
+        const website = "youtube";
+        console.log(username);
+    }
+    // console.log(website); // -> ReferenceError // -> not defined in one() scope
+    two();
+}
+
+// one(); // -> tinku
+
+if (true) {
+    const username = "tinku";
+    if (username === "tinku") {
+        const website = " youtube";
+        // console.log(username + website); // -> tinku youtube
+    }
+    // console.log(website); // -> ReferenceError // it is not defined in parent if block scope
+}
+
+// console.log(username); // -> reference error // not defined in global scope, present in if block scope only.. can't access outside
+
+
+
+// +++++++++++++++++ Interesting +++++++++++++++++++
+
+console.log(addOne(5)); // -> 6
+function addOne(num) { 
+    return num + 1;
+}
+
+
+console.log(addTwo(5)); // -> ReferenceError: Cannot access 'addTwo' before initialization  // Because of hoisting concept
+// Anonymous function: (function without any name)
+const addTwo = function (num) {
+    return num + 2;
+}
+
